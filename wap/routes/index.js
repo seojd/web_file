8262d2d3a421aef9f3abb222c566fc1ec4d9aab1
seojd <mylovema@naver.com>
2017-11-10 13:01:31 +0900

    수정6

--- wap/routes/index.js
+++ wap/routes/index.js
@@ -22,9 +22,14 @@ router.get('/enroll', function(req, res, next) {
   res.render('enroll', { title: '맛집등록', items:[] });
 });
 
-/* 맛집등록 */
+/* 맛집리뷰 */
 router.get('/review', function(req, res, next) {
   res.render('review', { title: '맛집리뷰', items:[] });
+});
+
+/* 맛집등록 */
+router.get('/redetail', function(req, res, next) {
+  res.render('redetail', { title: '맛집등록', items:[] });
 });
 
 // CREATE MEMBER
